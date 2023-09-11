91d46905fbfc55e91b546fabe6fa4d7bdf29d02d
joewatt95 <joewatt95@u.nus.edu>
2023-09-11 15:00:09 +0800

    Document script to build local docker img

--- build_and_run_dev_docker.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-
-docker build --tag cclawdev/logicalenglish:local-dev -f swish/Dockerfile .
-docker run -it -p 3050:3050  -e LOAD_KB=true -e SWISH_DAEMON_USER=root  --memory="300m" cclawdev/logicalenglish:local-dev
